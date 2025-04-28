c2fe824d3d6d6b1e5411a3d6885e7b3a02c25c09
Your Name <your-email@example.com>
2025-04-28 22:42:01 +0400

    /pages/Plan.tsx

--- src/app/subscribe/page.tsx
+++ src/app/subscribe/page.tsx
@@ -1,14 +1,21 @@
 "use client";
+
 import CreatePlan from "@/components/Subscribe/CreatePlanSection";
 import HowItWorksBlack from "@/components/Subscribe/HowItWorksBlack";
 import SubscriptionSection from "@/components/Subscribe/SubscriptionSection";
 
+const sections = [
+  CreatePlan,
+  HowItWorksBlack,
+  SubscriptionSection,
+];
+
 export default function Subscribe() {
   return (
     <div>
-      <CreatePlan />
-      <HowItWorksBlack />
-      <SubscriptionSection />
+      {sections.map((Section, index) => (
+        <Section key={index} />
+      ))}
     </div>
   );
 }
