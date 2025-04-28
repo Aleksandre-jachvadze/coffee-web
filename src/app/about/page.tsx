82965ef71b099840cf4b94ce42b7f4764a7557c6
Your Name <your-email@example.com>
2025-04-28 22:38:01 +0400

    /pages/Home.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,19 +5,13 @@ import CommitmentSection from "@/components/About/CommitmentSection";
 import QualitySection from "@/components/About/QualitySection";
 import HeadquartersSection from "@/components/About/HeadquartersSection";
 
-const sections = [
-  AboutUs,
-  CommitmentSection,
-  QualitySection,
-  HeadquartersSection,
-];
-
 export default function About() {
   return (
     <div>
-      {sections.map((Section, index) => (
-        <Section key={index} />
-      ))}
+      <AboutUs />
+      <CommitmentSection />
+      <QualitySection />
+      <HeadquartersSection />
     </div>
   );
 }
